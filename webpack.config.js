b87b19bb0104e55043f1ab02fe82139d771373e8
Nikolay Krishtopa <nickey87@yandex.ru>
2022-11-26 23:27:56 +0400

    dev/update webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -16,18 +14,18 @@ const config = {
     path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    open: true,
-    host: "localhost",
-    watchFiles: ["*.html"],
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8080,
     hot: true,
+    watchFiles: ["*.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
@@ -47,9 +45,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
