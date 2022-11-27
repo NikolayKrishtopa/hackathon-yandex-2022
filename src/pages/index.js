fd4e0346497038cbd26e134572dbc97178ae0fdd
Nikolay Krishtopa <nickey87@yandex.ru>
2022-11-27 12:38:45 +0400

    feat/add eraser and some adaptive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,25 @@
 import './index.css'
+
+const header = document.querySelector('.header')
+const headerButton = document.querySelector('.header__button')
+const headerNavbar = document.querySelector('.header__navbar')
+const eraserContainer = document.querySelector('.eraserContainer')
+
+header.addEventListener("mousemove", e => {
+  if (e.target === headerButton || e.target === headerNavbar) return
+  const eraser = document.createElement("span");
+  eraser.style.left = -50 + e.offsetX + "px";
+  eraser.style.top = -50 + e.offsetY + "px";
+  eraserContainer.appendChild(eraser);
+});
+
+
+// *** Надо подумать как реализовать отслеживание касания***
+
+// header.addEventListener("", e => {
+//   if (e.target === headerButton || e.target === headerNavbar) return
+//   const eraser = document.createElement("span");
+//   eraser.style.left = -50 + e.offsetX + "px";
+//   eraser.style.top = -50 + e.offsetY + "px";
+//   eraserContainer.appendChild(eraser);
+// });
