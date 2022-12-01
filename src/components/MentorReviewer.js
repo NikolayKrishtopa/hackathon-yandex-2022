b794ce5af80cd4744b99172704c5ffa8fa7631b9
Nikolay Krishtopa <nickey87@yandex.ru>
2022-12-01 17:39:26 +0400

    feat/adaptive mentorReviewer

--- src/components/MentorReviewer.js
+++ src/components/MentorReviewer.js
@@ -12,7 +12,6 @@ export default class MentorReviewer {
     this._config = config;
     this._section = document.querySelector(this._config.section);
     this._title = document.querySelector(this._config.title);
-    this._picture = document.querySelector(this._config.picture);
     this._icons = document.querySelector(this._config.icons);
     this._personName = document.querySelector(this._config.personNameSelector);
     this._mentorIcon = document.querySelector(this._config.mentorIcon);
@@ -46,8 +45,7 @@ export default class MentorReviewer {
 
   _showContentPerMode = () => {
     if (this._mode === modes.MENTOR) {
-      this._picture.src = mentorPic;
-      this._picture.alt = "Фото девушки";
+      this._section.style.background = mentorPic;
       this._mentorIcon.classList.remove(this._config.iconInactiveClass);
       this._reviewerIcon.classList.add(this._config.iconInactiveClass);
       this._quotesContainer.classList.remove(
@@ -65,9 +63,7 @@ export default class MentorReviewer {
         });
       });
     } else {
-      this._picture.src = reviewerPic;
-      this._picture.alt =
-        "Фото молодого парня за рабочим столом перед монитором";
+      this._section.style.background = reviewerPic;
       this._mentorIcon.classList.add(this._config.iconInactiveClass);
       this._reviewerIcon.classList.remove(this._config.iconInactiveClass);
       this._quotesContainer.classList.add(
